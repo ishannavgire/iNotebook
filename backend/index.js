8dc8edfa3c8c8cba507625806e77dff8d6250328
ishannavgire <ishannavgire@yahoo.com>
2021-09-13 23:31:34 +0530

    Session 45

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
+const { json } = require("express");
 const express = require("express");
 const connecttoMongo = require("./db.js");
 
 connecttoMongo();
 const app = express();
 const port = 3000;
+
+//Add middleware to work with request
+app.use(express.json());
 
 //Available Routes
 app.use("/api/auth", require("./routes/auth.js"));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.json([]);
+//Create a User using: POST "/api/auth/". Doesn't require Auth
+router.post("/", (req, res) => {
+  const user = User(req.body);
+  user.save();
+  res.json("User added to db successfully!");
 });
 
 module.exports = router;
