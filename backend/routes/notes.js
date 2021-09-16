ac748511f8428aa0a6522c1dd972c82ea23d4bc4
ishannavgire <ishannavgire@yahoo.com>
2021-09-16 12:07:58 +0530

    Session 54 - added endpoint to delete a note.

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -76,4 +76,26 @@ router.put("/updatenote/:id", fetchuser, async (req, res) => {
   }
 });
 
+//ROUTE 4: Delete an existing note using: DELETE "/api/notes/deletenote". Login required.
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+  try {
+    //Find the note to be deleted and delete it
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).json({ error: "Not found." });
+    }
+
+    //Validate user
+    if (note.user.toString() !== req.user.id) {
+      return res.status(404).json({ error: "Not Allowed." });
+    }
+
+    note = await Notes.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Note successfully deleted.", note: note });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal server error." });
+  }
+});
+
 module.exports = router;
