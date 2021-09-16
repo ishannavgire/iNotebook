4d23de865a06b4f19d7a267d276d37dd9ab0e94c
ishannavgire <ishannavgire@yahoo.com>
2021-09-16 16:08:55 +0530

    Session 56 - Add Home, About, Navbar components and add Router.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Home } from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div>
+          <Navbar />
+          <div className="scrollable" style={{ width: "100%", top: "500px", maxHeight: "1000px", overflow: "auto" }}>
+            <Switch>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/home">
+                <Home />
+              </Route>
+              <Route exact path="/"></Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </>
   );
 }
 
